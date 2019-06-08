655aba9f4165865f58074dbe091c992b8234bf32
Vilas Magare <vilas@numinolabs.com>
2019-06-09 00:28:24 +0530

    added component.js

---
+++ components.js
@@ -0,0 +1,109 @@
+
+
+class ChAi extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+
+window.customElements.define('ch-ai', ChAi);
+
+class CofFee extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+
+window.customElements.define('cof-fee', CofFee);
+
+class HeAd extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+window.customElements.define('he-ad', HeAd);
+
+class ConTeaNt extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+window.customElements.define('con-tea-nt', ConTeaNt);
+
+class LeftConTeaNt extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+window.customElements.define('left-con-tea-nt', LeftConTeaNt);
+
+class LeftWest extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+window.customElements.define('left-west', LeftWest);
+
+class LeftEast extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+window.customElements.define('left-east', LeftEast);
+
+class DashboardHeader extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+window.customElements.define('dashboard-header', DashboardHeader);
+
+class DashBoard extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+window.customElements.define('dash-board', DashBoard);
+
+class BarChart extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+window.customElements.define('bar-chart', BarChart);
+
+class BubBle extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+window.customElements.define('bub-ble', BubBle);
+
+class TAble extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+window.customElements.define('t-able', TAble);
+
+class RightConTeaNt extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+window.customElements.define('right-con-tea-nt', RightConTeaNt);
+
+class CounterHeader extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+window.customElements.define('counter-header', CounterHeader);
+
+class CounterTitle extends HTMLElement {
+  constructor() {
+    super();
+    let self = this;
+  }
+}
+window.customElements.define('counter-title', CounterTitle);
